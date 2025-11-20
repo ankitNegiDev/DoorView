fd2ab5d7d754a38b94faa0a1f9cb89c64bfd5b66
ankitNegiDev <ankitnegi9104@gmail.com>
2025-11-20 10:04:02 +0530

    Update globals.css, layout.tsx, and page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,79 @@
-import Image from "next/image";
+import CallForm from "./components/CallForm";
+import ImageSwiper from "./components/ImageSwiper";
+import MoreDoorIdeas from "./components/MoreDoorIdeas";
+import OtherProduct from "./components/OtherProduct";
+import Sidebar from "./components/Sidebar";
+import Tab from './components/Tab';
+import YouTubeEmbed from './components/YouTubeEmbed';
+import Footer from "./components/Footer";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    const images=["/internship task 1 image.jpg", "/internship task 2 iamge.jpg","/internship task 3 image.jpg","/internship task 4 image.jpg"];
+    const doorImages = [
+        "/internship task 1 image.jpg",
+        "/internship task 2 iamge.jpg",
+        "/internship task 3 image.jpg",
+    ];
+    return (
+        <>
+            <div className="flex p-4">
+                {/* left */}
+                <div className="w-[45%] flex flex-col gap-10 mr-4">
+                    <Sidebar />
+                    <OtherProduct
+                        image={"/Bamboo.jpg"}
+                        text={"Bamboo Iron Security Door"}
+                    />
+                    <OtherProduct
+                        image={"/iron.jpg"}
+                        text={"Custom French Exclusive Iron Entry Door"}
+                    />
+                    <OtherProduct
+                        image={"/iron2.jpg"}
+                        text={"Desert Sunray Iron Entry Door"}
+                    />
+                    <OtherProduct
+                        image={"/iron3.jpg"}
+                        text={"Desert Sunray Iron Security Door"}
+                    />
+                </div>
+
+                <div className="w-full flex flex-col">
+                    {/* Horizontal row: Swiper + More Ideas */}
+                    <div className="flex w-full gap-4 mb-6">
+                        {/* LEFT: Image Swiper */}
+                        <div className="w-3/4">
+                            <h1 className="ml-4 text-4xl font-semibold text-gray-800 mb-4 tracking-tight">
+                                Custom Iron Security Door
+                            </h1>
+
+                            <ImageSwiper images={images} />
+                        </div>
+
+                        {/* RIGHT: More Door Ideas */}
+                        <div className="w-1/4">
+                            <MoreDoorIdeas doorImages={doorImages} />
+                        </div>
+                    </div>
+
+                    {/* Tab Section below */}
+                    <div className="w-full">
+                        <Tab />
+                    </div>
+
+                    {/* yt embedded video */}
+                    <div>
+                        <YouTubeEmbed />
+                    </div>
+                </div>
+                {/* right  */}
+                <div>
+                    <CallForm />
+                </div>
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </>
+    );
 }
